cca04a25d1a38377d37f57cd8e8f566d575ffbda
vineas <alvienasyandika@gmail.com>
2024-08-08 23:45:12 +0700

    array tuple

--- bundle/index.js
+++ bundle/index.js
@@ -6,3 +6,31 @@ console.log(namaSaya);
 console.log({ namaSaya }); //dengan kurung kurawal akan ada keterangan pada terminalnya
 console.log({ userName });
 console.log({ isDead });
+/**
+ * ts wajib menggunakan tipe data. jika seperti ini, maka akan default bertipe data "any"
+ * maka akan bentrok nantinya
+ */
+// let pacarSaya; 
+// pacarSaya= "Susanti";
+// pacarSaya= 1;
+// console.log(pacarSaya);
+/**
+ * seharusnya seperti ini
+ */
+let pacarSaya; //berikan langsung tipe data nya, jika ingin diantara kedua tipe data
+pacarSaya = "Susanti";
+pacarSaya = 1;
+pacarSaya = true;
+console.log({ pacarSaya });
+/**
+ * Misal kasusnya array (array biasa)
+ */
+let istriSaya; //maka tipe data arraynya (array biasa) harus dibuat seperti ini
+istriSaya = ["Siti", "Sita"]; //karena isi array nya berupa string
+console.log({ istriSaya });
+/**
+ * Array tuple type
+ */
+let gebetanSaya;
+gebetanSaya = ["Siti", 23, false];
+console.log({ gebetanSaya });
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,45 @@ let namaSaya: string = "Alvienas Yandika";
 let userName: number = 123;
 let isDead: boolean = false;
 
-let pacarSaya = "ada banyak";
-
 console.log(namaSaya);
 console.log({namaSaya}); //dengan kurung kurawal akan ada keterangan pada terminalnya
 console.log({userName});
 console.log({isDead});
+
+/**
+ * ts wajib menggunakan tipe data. jika seperti ini, maka akan default bertipe data "any"
+ * maka akan bentrok nantinya
+ */
+// let pacarSaya; 
+// pacarSaya= "Susanti";
+// pacarSaya= 1;
+
+// console.log(pacarSaya);
+
+/**
+ * seharusnya seperti ini
+ */
+let pacarSaya: number | string | boolean; //berikan langsung tipe data nya, jika ingin diantara kedua tipe data
+pacarSaya= "Susanti"; 
+pacarSaya= 1;
+pacarSaya = true
+
+console.log({pacarSaya});
+
+/**
+ * Misal kasusnya array (array biasa)
+ */
+let istriSaya: string[]; //maka tipe data arraynya (array biasa) harus dibuat seperti ini
+istriSaya = ["Siti", "Sita"]; //karena isi array nya berupa string
+console.log({istriSaya});
+
+/**
+ * Array tuple type
+ */
+let gebetanSaya: [string, number, boolean];  //penulisannya seperti ini
+gebetanSaya = ["Siti", 23, false]; 
+console.log({gebetanSaya});
+
+/** 
+ * Menit 24:00
+*/
