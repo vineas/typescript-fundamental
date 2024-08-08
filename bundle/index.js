85bc7b34cd4ce2ecbe598c0cd0063072a457fb40
vineas <alvienasyandika@gmail.com>
2024-08-08 23:11:55 +0700

    first commit

---
+++ bundle/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let namaSaya = "Alvienas Yandika";
+let userName = 123;
+let isDead = false;
+console.log(namaSaya);
+console.log({ namaSaya }); //dengan kurung kurawal akan ada keterangan pada terminalnya
+console.log({ userName });
+console.log({ isDead });
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let namaSaya: string = "Alvienas Yandika";
+let userName: number = 123;
+let isDead: boolean = false;
+
+let pacarSaya = "ada banyak";
+
+console.log(namaSaya);
+console.log({namaSaya}); //dengan kurung kurawal akan ada keterangan pada terminalnya
+console.log({userName});
+console.log({isDead});
